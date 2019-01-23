c0734182eac66d0815684344fb6f95b49eeb1d46
Balint Gyapjas <balint@gyapjas.de>
2019-01-23 18:19:56 +0100

    key binding for tab-move and download-open

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -5,24 +5,27 @@
 # Type: Bool
 c.auto_save.session = True
 
-# bookmarks
+# Bookmarks
 config.bind('a', 'set-cmd-text -s :bookmark-add {url}')
 config.bind('A', 'bookmark-add -t')
 config.bind('b', 'set-cmd-text -s :bookmark-load')
 config.bind('B', 'set-cmd-text -s :bookmark-load -t')
 
-# Message timeout
+
+# Keys
+config.bind("O", "set-cmd-text -s :open -rt")
+# open download
+config.bind("<ctrl-o>", "download-open")
+# move tabs around
+config.bind("<alt-shift-k>", "tab-move -")
+config.bind("<alt-shift-r>", "tab-move +")
+
+
+# Looks
+c.scrolling.bar = "always"
 c.messages.timeout = 6000
-
-# Tabs 
-c.fonts.tabs = '12'
-c.tabs.background = True
-c.tabs.padding = {'top': 1, 'bottom': 2, 'left': 5, 'right': 5}
-
-c.tabs.last_close = 'blank'
-
-# Zoom
 c.zoom.default = '125'
+
 
 # Search engines
 c.url.searchengines = {
@@ -32,9 +35,10 @@ c.url.searchengines = {
     "gmaps" : "http://maps.google.com/?q={}",
 }
 
-#
-c.scrolling.bar = "always"
 
-## Keybinding
-# new tab is opened next to current tab
-config.bind("O", "set-cmd-text -s :open -rt")
+# Tabs 
+c.fonts.tabs = '12'
+c.tabs.background = True
+c.tabs.padding = {'top': 1, 'bottom': 2, 'left': 5, 'right': 5}
+
+c.tabs.last_close = 'blank'
